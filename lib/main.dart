cd8b3d5339ef093731fb41bde50a946d847c1959
Mohamed Mahmoud <mohamedelshenawy622@gmail.com>
2023-02-16 07:56:25 -0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:salon_app/shared/componants/color_manager.dart';
 
-import 'modules/onboarding/onBoardig_screen.dart';
+import 'modules/onboarding/onboarding_screen.dart';
 
-
-void main()async {
-
-  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark,statusBarColor: Colors.white));
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: ColorManager.scaffoldBackgroundColor));
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return   ScreenUtilInit(
+    return ScreenUtilInit(
       builder: (BuildContext context, Widget? child) {
-        return  const MaterialApp(
-
+        return const MaterialApp(
           debugShowCheckedModeBanner: false,
-
           home: OnBoardingScreen(),
         );
       },
     );
   }
 }
-
